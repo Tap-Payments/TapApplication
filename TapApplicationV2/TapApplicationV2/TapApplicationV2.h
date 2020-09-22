a6d4da540247538421c5024bcfe98550aa7f21e0
O Rabie <o.rabie@tap.company>
2020-09-22 11:32:10 +0200

    Upgrade to Xcode12+ compiler friendly code

---
+++ TapApplicationV2/TapApplicationV2/TapApplicationV2.h
@@ -0,0 +1,18 @@
+//
+//  TapApplicationV2.h
+//  TapApplicationV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapApplicationV2.
+FOUNDATION_EXPORT double TapApplicationV2VersionNumber;
+
+//! Project version string for TapApplicationV2.
+FOUNDATION_EXPORT const unsigned char TapApplicationV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapApplicationV2/PublicHeader.h>
+
+
